722a76c428f024d0aaa577287bf588c9a02e7a1d
Dean Orion <60265116+Doer64@users.noreply.github.com>
2024-11-25 22:32:57 +0200

    Fixed MyString

--- MyString.java
+++ MyString.java
@@ -30,6 +30,7 @@ public class MyString {
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
+        if(str2.length() == 0) return true;
         for(int i = 0; i < str1.length(); i++){
             if(str1.charAt(i) == str2.charAt(0) && (str1.length() > (i+str2.length()-1))){
                 boolean whole = true;
